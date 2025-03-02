0c6fd15018318974fb708b7f4dd5a8edacbbe024
Vladik <ignatov.vlad04@gmail.com>
2025-03-02 17:33:43 +0700

    Destroy + Transparent scripts

--- Assets/LearnMaterials 2/Scripts/DestroyModule.cs
+++ Assets/LearnMaterials 2/Scripts/DestroyModule.cs
@@ -3,22 +3,21 @@ using UnityEngine;
 
 [HelpURL("https://docs.google.com/document/d/1RMamVxE-yUpSfsPD_dEa4-Ak1qu6NTo83qY1O4XLxUY/edit?usp=sharing")]
 public class DestroyModule : MonoBehaviour
-{
+{   
+    [Tooltip("Задержка перед уничтожением следующего объекта")]
+    [SerializeField, Min(0)]
     private float destroyDelay;
+    [SerializeField, Min(0)]
     private int minimalDestroyingObjectsCount;
 
-    private Transform myTransform;
-
-    private void Awake()
-    {
-        myTransform = transform;
-    }
-
+    private Transform myTransform => transform;
+    
+    [ContextMenu("Начать удаление объектов")]
     public void ActivateModule()
     {
         StartCoroutine(DestroyRandomChildObjectCoroutine());
     }
-
+    
     private IEnumerator DestroyRandomChildObjectCoroutine()
     {
         while (myTransform.childCount > minimalDestroyingObjectsCount)
--- Assets/LearnMaterials 2/Scripts/TransparentModule.cs
+++ Assets/LearnMaterials 2/Scripts/TransparentModule.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1Cmm__cbik5J8aHAI6PPaAUmEMF3wAcNo3rpgzsYPzDM/edit?usp=sharing")]
 public class TransparentModule : MonoBehaviour
 {
+    [SerializeField, Min(0)]
     private float changeSpeed;
 
     private float defaultAlpha;
@@ -19,6 +20,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Начать изменение")]
     public void ActivateModule()
     {
         float target = toDefault ? defaultAlpha : 0;
